3cdda4de5ed9b7e5f45f8828ecd753c9a59f8fa1
Harshada <harshada.t1998@gmail.com>
2020-05-26 20:23:17 +0530

    Added call to service x.
    Added a call to service x so that we can get JSON response from the
    remote service.

--- astpath/cli.py
+++ astpath/cli.py
@@ -17,6 +17,7 @@ from astpath.search import search
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--hide-lines', help="hide source lines, showing only line numbers", action='store_true',)
+parser.add_argument('-q', '--quiet', help="hide output of matches", action='store_true',)
 parser.add_argument('-v', '--verbose', help="increase output verbosity", action='store_true',)
 parser.add_argument('-a', '--abspaths', help="show absolute paths", action='store_true',)
 parser.add_argument('-R', '--no-recurse', help="ignore subdirectories, searching only files in the specified directory", action='store_true',)
@@ -48,11 +49,12 @@ def main():
         args.dir,
         ' '.join(args.expr),
         show_lines=not args.hide_lines,
+        print_matches=not args.quiet,
         verbose=args.verbose,
         abspaths=args.abspaths,
         recurse=recurse,
         before_context=before_context,
-        after_context=after_context
+        after_context=after_context,
     )
 
 
